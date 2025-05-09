b234f4298e688965a233fc7bb4af6a885ae1791c
ludwink <141805216+ludwink@users.noreply.github.com>
2025-05-09 12:45:18 -0600

    feat(server): add initial server setup with environment config and routing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+use std::env;
+
+use dotenvy::dotenv;
+use tokio::net::TcpListener;
+
+mod router;
+
+use router::router;
+
+#[tokio::main]
+async fn main() {
+    // Load .env file (development mode)
+    dotenv().ok();
+
+    // TcpListener
+    let port: u16 = env::var("PORT")
+        .expect("PORT undefined")
+        .parse()
+        .expect("PORT value must be u16");
+
+    let listener = TcpListener::bind(format!("0.0.0.0:{}", port))
+        .await
+        .expect("Failed to create TcpListener to specified address");
+
+    // Start server
+    println!("Server running on port: {}", listener.local_addr().unwrap());
+
+    axum::serve(listener, router())
+        .await
+        .expect("Failed to start Axum server")
 }
